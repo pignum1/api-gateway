d8f748e47abebadf388c92684d0b994122d9292e
weixy <827928660@qq.com>
2019-07-01 11:09:33 +0800

    test

--- src/main/java/com/demo/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/demo/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,12 @@ package com.demo.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
