cbba594f2783e928ef4ef9c2f971822b27da5e58
weixy <827928660@qq.com>
2019-08-02 16:47:25 +0800

    请求过滤和注册中心显示服务名称

--- src/main/java/com/demo/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/demo/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.demo.apigateway;
 
+import com.demo.apigateway.zuul.ZuulFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	ZuulFilter getZuulFilter() {
+		return new ZuulFilter();
 	}
 
 }
